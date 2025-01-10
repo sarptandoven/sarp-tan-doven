7b0fefc1e8eaddb345cfa98b07038ac546ea148f
sarptandoven <119648987+sarptandoven@users.noreply.github.com>
2025-01-09 23:04:39 -0500

    Initial commit from my VS code updates to my website!

---
+++ readme.md
@@ -0,0 +1,3 @@
+#This is my personal portfolio!
+
+It will be hosted soon :0
---
+++ src/script.js
@@ -0,0 +1,5 @@
+import "./style.css";
+import Desktop from "./desktop";
+
+const desktop = new Desktop();
+desktop.activateEvents();
